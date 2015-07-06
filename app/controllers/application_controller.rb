36063d2fb0fe97e4031d960f8cf3407a70d4d9c9
InkCube <birgit.klemenz@gmail.com>
2015-07-07 00:59:29 +0200

    fix test method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def test
-    render "This is a test"
+    render text:"This is a test"
   end
 end
