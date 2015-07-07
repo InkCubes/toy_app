5d5190716828706e327dd7e45045694a43a1faa3
InkCube <birgit.klemenz@gmail.com>
2015-07-07 12:34:55 +0200

    new try to redirect

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   #root 'application#test'
-  root :to => redirect('/users')
+  #root :to => redirect('/users')
+  root 'users#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
